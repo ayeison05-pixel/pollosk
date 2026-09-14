1df69d22f24d574fbe2af5d00dcc192b5fdc01f5
ayeison05-pixel <a.yeison05@gmail.com>
2026-09-14 16:44:01 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pollos-k-cache-v3'; // ← Solo cambia esto (v1 → v2)
+const CACHE_NAME = 'pollos-k-cache-v4'; // ← Solo cambia esto (v1 → v2)
 const urlsToCache = [
   './index.html',
   './manifest.json',
